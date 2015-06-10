361fd81c5f8113aad81fd2ae1fc608078653d8b6
Artur Tsuda <artur.o.tsuda@gmail.com>
2015-06-10 18:44:40 -0300

    lexicon nosetests

---
+++ projects/ex48/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Artur Tsuda',
+    'url': '',
+    'download_url': '',
+    'author_email': 'artur.o.tsuda@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex48'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
